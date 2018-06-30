8dc3a7ff1464c216c544580ea20ee151a743a545
dakpek <dorukakpek@gmail.com>
2018-06-30 12:45:27 +0200

    userprofile

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -4,11 +4,10 @@ import './userProfile.css';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import {getWallets} from '../../actions';
-
 import { Layout, Menu } from 'antd';
-
+import CreateWallet from '../createWallet';
+import SelectedWallet from '../../containers/selectedWallet';
 const { Header, Content, Footer, Sider } = Layout;
-
 
 //users profile component, create a link to a creation of new wallet and its redirect, append all existing wallets of user
 class UserProfile extends Component {
@@ -16,13 +15,15 @@ class UserProfile extends Component {
   constructor (props) {
     super(props);
     this.getWallets();
+    this.state = {
+      view : null
+    };
   }
 
   getWallets = () => {
     fetch('http://192.168.1.241:3030/wallet',
       {
         headers:{
-
           'Authorization':'Bearer '+this.props.userLogged.jwt
         }
       }
@@ -33,22 +34,24 @@ class UserProfile extends Component {
   }
 
   handleOnClick = (e) => {
+    console.log(this.state);
     this.setState({
       view:e
-    })
+    }, ()=> console.log(this.state));
   }
 
   renderWallets = () => {
     if(this.props.renderWallets.wallets && this.props.renderWallets.wallets.length) {
       return this.props.renderWallets.wallets.map(e => {
         return (
-          <Menu.Item key={Math.random()} >
-            <a onClick={this.selectedWallet}>
+
+          <Menu.Item key={e.publickey} >
+            <div onClick={() => this.handleOnClick(e)}>
               <div className='userprofile-menuitem'>
-                <p>{e.alias}</p>
+                <p >{e.alias}</p>
                 <p>{e.balance/1000000000}</p>
               </div>
-            </a>
+            </div>
           </Menu.Item>
         );
       });
@@ -70,64 +73,11 @@ class UserProfile extends Component {
             </Menu>
           </Sider>
           <Layout style={{ marginLeft: 200 }}>
-            <Header style={{ background: '#fff', padding: 0 }} />
-            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-              <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-                <p>.</p>
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
-            </Footer>
+            {this.selectedWallet()}
           </Layout>
         </Layout>
       </div>
-      // <div className="userprofile-father">
-      //   <div className="userprofile-child-one">
-      //     <div className="userprofile-background-image">
-      //
-      //       <button className="button-create-wallet">
-      //         Create a new Wallet
-      //       </button>
-      //     </div>
-      //   </div>
-      //   <div className="userprofile-child-two">
-      //     {this.renderWallets()}
-      //   </div>
-      // </div>
+
     );
   }
 }
@@ -149,6 +99,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = (dispatch) => ({
 
   getWallets: data => dispatch(getWallets(data)),
+
 
 });
 
