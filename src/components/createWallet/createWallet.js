76cc12733838c5aa6d3c2a02b2b2f6e0df49a654
jportella93 <jportella93@gmail.com>
2018-07-02 19:01:05 +0200

    feat:add users when creating wallet (needs css)

--- src/components/createWallet/createWallet.js
+++ src/components/createWallet/createWallet.js
@@ -5,10 +5,29 @@ import PropTypes from 'prop-types';
 
 
 class CreateWallet extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      alias: '',
+      user: null,
+      users: []
+    };
+  }
+
   handleOnSubmit = (event) => {
     event.preventDefault();
     event.target.reset();
-    this.props.fetchCreateWallet(this.state.walletName);
+    const data = {
+      alias: this.state.alias,
+      users: this.state.users
+    };
+    this.props.fetchCreateWallet(data);
+  }
+
+  captureUser = e => {
+    this.setState({
+      user: e.target.value
+    });
   }
 
   captureInput = e => {
@@ -17,23 +36,53 @@ class CreateWallet extends Component {
     });
   }
 
+  pushUser = () => {
+    this.setState({
+      users: [...this.state.users, this.state.user]
+    });
+  }
+
+  deleteUser = (userToDelete) => {
+    this.setState({
+      users: this.state.users.filter(user => user !== userToDelete)
+    });
+  }
+
   render () {
     return (
       <div className="createWallet">
         <form className="createWallet_form" onSubmit={this.handleOnSubmit}>
           <input
-            name="walletName"
+            name="alias"
             type="text"
             className="createWallet_form_wallet-name"
             placeholder="Name of the wallet to create"
             size="30"
             onChange={this.captureInput}
+            required
           />
           <input
             type='submit'
             value='Create Wallet'
           />
         </form>
+        <button onClick={this.pushUser}>Add User</button>
+        <input
+          name="users"
+          type="text"
+          placeholder="Add username to the wallet"
+          onChange={this.captureUser}
+        >
+        </input>
+        <ul>
+          {this.state.users.length > 0
+            ? this.state.users.map(user => {
+              return <div key={user}>
+                <button onClick={() => this.deleteUser(user)}>❌</button>
+                <li>{user}</li>
+              </div>;})
+            : null}
+        </ul>
       </div>
     );
   }
@@ -49,9 +98,7 @@ const mapDispatchToProps = (dispatch) => ({
     [API]: {
       path: '/wallet',
       method: 'POST',
-      body: {
-        alias: data
-      }
+      body: data
     }
   })
 });
