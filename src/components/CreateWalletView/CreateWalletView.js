9aa73065e87c5608ccdb62f9205997368fdf8b06
dakpek <dorukakpek@gmail.com>
2018-07-04 14:01:04 +0200

    create wallet button in the middle, above the spinning earth. form next

--- src/components/CreateWalletView/CreateWalletView.js
+++ src/components/CreateWalletView/CreateWalletView.js
@@ -9,7 +9,7 @@ class CreateWalletView extends Component {
       <div className="CreateWalletView">
         {this.props.form
           ? <CreateWallet />
-          : <button onClick={this.props.handleOnClick}>Create Wallet</button>}
+          : <button className='cw-create-wallet-button' onClick={this.props.handleOnClick}>Create Wallet</button>}
       </div>
     );
   }
