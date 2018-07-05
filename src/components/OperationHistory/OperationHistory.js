6326f663da80c22b23f789ffb2aa0a6f61fb0ae6
dakpek <dorukakpek@gmail.com>
2018-07-05 12:46:56 +0200

    transaction filters

--- src/components/OperationHistory/OperationHistory.js
+++ src/components/OperationHistory/OperationHistory.js
@@ -3,6 +3,48 @@ import PropTypes from 'prop-types';
 import './OperationHistory.css';
 
 class OperationHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: {
+        Approved: true,
+        Pending: true,
+        Rejected: true
+      },
+    }
+  }
+
+  operationStateSwitcher = (state) => {
+    let stateCopy;
+    switch (state) {
+    case 'Approved':
+      stateCopy = Object.assign({}, this.state);
+      stateCopy.filter.Approved = !this.state.filter.Approved;
+      this.setState(stateCopy);
+      console.log(this.state.filter);
+    break;
+    case 'Pending':
+      stateCopy = Object.assign({}, this.state);
+      stateCopy.filter.Pending = !this.state.filter.Pending;
+      this.setState(stateCopy);
+      console.log(this.state.filter);
+    break;
+    case 'Rejected':
+      stateCopy = Object.assign({}, this.state);
+      stateCopy.filter.Rejected = !this.state.filter.Rejected;
+      this.setState(stateCopy);
+      console.log(this.state.filter);
+    break;
+    }
+  }
+
+  renderOperationFilterer = () => {
+    let filter = []
+    this.props.operations.forEach(operation => {
+      if(this.state.filter[operation.result] === true) filter.push(operation)
+    })
+    return filter;
+  }
 
   renderOperationResultIcon = (result) => {
     switch (result) {
@@ -35,14 +77,54 @@ class OperationHistory extends Component {
   }
 
   render () {
+    console.log(this.state.filter);
     if (this.props.operations.length < 1) return <h3 id='no-operations'>No operations yet</h3>;
     return (
       <div className="OperationHistory">
-        <div className='OperationHistory'>
+        <div className='OperationHistoryTitle'>
           <h2>History of operations</h2>
+          <div className='filterButtons'>
+          <div className='Approved'>
+            <button
+              className='operationHistory-filerButton-approved'
+              name='Approved'
+              style={this.state.filter.Approved
+                    ? {'background-color': 'rgba(56,175,91,0.7)'}
+                    : {'background-color': 'transparent'}
+                    }
+              onClick={() => this.operationStateSwitcher('Approved')}
+            >
+            </button>
+          </div>
+          <div className='Pending'>
+            <button
+              className='operationHistory-filerButton-pending'
+              name='Pending'
+              style={this.state.filter.Pending
+                    ? {'background-color': 'rgba(234,142,5, 0.7)'}
+                    : {'background-color': 'transparent'}
+                    }
+              onClick={() => this.operationStateSwitcher('Pending')}
+              >
+              </button>
+          </div>
+          <div className='Rejected'>
+            <button
+              className='operationHistory-filerButton-rejected'
+              name='Rejected'
+              style={this.state.filter.Rejected
+                    ? {'background-color': 'rgba(236,72,127,0.7)'}
+                    : {'background-color': 'transparent'}
+                    }
+              onClick={() => this.operationStateSwitcher('Rejected')}
+              >
+              </button>
+          </div>
+          </div>
         </div>
+
         <div className='OperationHistory-grid'>
-          {this.props.operations.map(operation => {
+          {this.renderOperationFilterer().map(operation => {
             return <div key={operation.operation_id + operation.closed_at}
               className={(operation.result==='Approved') ? 'OperationHistory_operation3':(operation.result==='Rejected') ? 'OperationHistory_operation1':'OperationHistory_operation2'}>
               <h3>{`Type: ${this.renderOperationType(operation.type)}`}</h3>
